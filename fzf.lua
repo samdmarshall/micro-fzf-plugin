fe4e18b111ed2eac25ea6b0f54d04baf6e35ea9e
Samantha Marshall <me@samdmarshall.com>
2016-11-28 11:41:33 -0500

    initial commit of fzf plugin for micro

---
+++ fzf.lua
@@ -0,0 +1,30 @@
+VERSION = "1.0"
+
+MakeCommand("fzf", "fzf.fzf", 0)
+
+function removeNewlines(string_value)
+	if string.find(string_value, "\n") then
+		return string.gsub(string_value, "\n", "")
+	else
+		return string_value
+	end
+end
+
+function reportedExitCode(string_value)
+	local exit_code_string = "exit status "
+	return string.sub(string_value, 1, string.len(exit_code_string)) == exit_code_string
+end
+
+function fzf()
+	CurView():Save(false)
+	local fzf_output = HandleShellCommand("fzf", true, false)
+	if not reportedExitCode(fzf_output) then
+		local desired_path = removeNewlines(fzf_output)
+		local working_path = removeNewlines(HandleShellCommand("pwd", false, false))
+		local selected_path = working_path .. desired_path
+		CurView():Open(selected_path)
+		messenger:Message("Opened "..selected_path)
+	end
+end
+
+AddRuntimeFile("fzf", "help", "help/fzf.md")
