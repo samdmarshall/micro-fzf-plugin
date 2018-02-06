fffc484ba5f25de248ed0d6177f034b789e9a3ad
zcotter@forwardfinancing.com <jaffecotter.z@gmail.com>
2018-02-05 21:26:03 -0500

    Add support for micro 1.3.5 and above

--- fzf.lua
+++ fzf.lua
@@ -1,29 +1,16 @@
-VERSION = "1.0.5"
+VERSION = "2.0.0"
 
 MakeCommand("fzf", "fzf.fzf", 0)
 
-function removeNewlines(string_value)
-	if string.find(string_value, "\n") then
-		return string.gsub(string_value, "\n", "")
-	else
-		return string_value
-	end
-end
-
-function reportedExitCode(string_value)
-	local exit_code_string = "exit status "
-	return string.sub(string_value, 1, string.len(exit_code_string)) == exit_code_string
-end
-
 function fzf()
-	if CurView():CanClose() then
-		local fzf_output = HandleShellCommand("fzf", true, false)
-		if not reportedExitCode(fzf_output) then
-			local desired_path = removeNewlines(fzf_output)
-			CurView():Open(desired_path)
-			messenger:Message("Opened "..desired_path)
-		end
-	end
+  if CurView():CanClose() then
+    RunTermEmulator("fzf", false, true, "fzf.handleFzfOutput")
+  end
+end
+
+function handleFzfOutput(output)
+  local strings = import("strings")
+  CurView():Open(strings.TrimSpace(output))
 end
 
 AddRuntimeFile("fzf", "help", "help/fzf.md")
